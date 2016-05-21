3a34a34b2ef0d362dda4540a1c968c5383f09ac6
dyno <oldman.mtb@gmail.com>
2016-05-21 01:24:17 -0700

    try android studio2.2 the ndk-build support is not good

--- git_deps.sh
+++ git_deps.sh
@@ -1,4 +1,3 @@
 git submodule add https://github.com/dropbox/djinni.git deps/djinni
 git submodule add https://chromium.googlesource.com/external/gyp.git deps/gyp
-cd deps/gyp
-git checkout -q 0bb67471bca068996e15b56738fa4824dfa19de0
+cd deps/gyp &&git checkout -q 0bb67471bca068996e15b56738fa4824dfa19de0
